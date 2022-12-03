05499b44723ac8cf7d05c71ea3ba3f88fce110c5
ShafiqAhmed123 <skillsispower321@gmail.com>
2022-12-03 19:09:40 +0500

    first commit

---
+++ han.js
@@ -0,0 +1,11 @@
+
+function login(){
+    document.getElementById("login").style.display='none'
+    document.getElementById("signup").style.display='bloxk'
+
+}
+
+function signup(){
+    document.getElementById("signup").style.display='none'
+    document.getElementById("login").style.display='block'    
+}
